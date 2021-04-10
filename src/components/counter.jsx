b33011d423b44d0ecad48eef252b4dc80b1be3a5
Gracious <graciousnguwo56@gmail.com>
2021-04-11 00:49:57 +0200

    style

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,13 +3,23 @@ import React, { Component } from "react";
 class Counter extends Component {
     state = {
         count: 0,
+        // imageUrl: "https://picsum.photos/100"
 
     };
+
+    // styles = {
+    //   fontSize: 10,
+    //   fontWeight: 'bold'
+
+    // };
+
   render() {
     return (
       <div>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        {/* <img src={this.state.imageUrl} alt="" /> */}
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2" >{this.formatCount()}</span>
+        <span style={this.styles} className="badge badge-primary m-2" >{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
