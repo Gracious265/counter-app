cc7a9a82c1cb569222459ca971dae4352a474d1a
Gracious <graciousnguwo56@gmail.com>
2021-04-11 22:49:48 +0200

    edited

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,27 +2,41 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state = {
-        count: 0,
-        // imageUrl: "https://picsum.photos/100"
-
+        count: 0
+      
     };
 
-    // styles = {
-    //   fontSize: 10,
-    //   fontWeight: 'bold'
+    // constructor() {
+    //   super();
+    //   this.handleIncrement = this.handleIncrement.bind(this);
+    // }
 
-    // };
+    handleIncrement = product => {
+      console.log(product);
+      this.setState({ count: this.state.count +1 });
+      // obj.method();
+      // function();
+      //this.handleIncrement = this.handleIncrement.bind(this);
 
+    };
+    
+    
   render() {
+    
     return (
       <div>
-        {/* <img src={this.state.imageUrl} alt="" /> */}
-        <span style={{ fontSize: 30 }} className="badge badge-primary m-2" >{this.formatCount()}</span>
-        <span style={this.styles} className="badge badge-primary m-2" >{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button onClick={ () => this.handleIncrement(product)} className="btn btn-secondary btn-sm">Increment</button>
       </div>
+      
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
       const { count } = this.state;
       return count === 0 ? 'Zero' : count;
